8ec3782da84ff7f9a71ce921e8efe3c6f9bff963
Jon-Martin Storm <jonmartin.storm@gmail.com>
2024-10-10 08:05:23 +0200

    testing connection to Ollama, works.

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-from dotenv import load_dotenv
+from dotenv import dotenv_values
+from langchain_ollama import ChatOllama
+from langchain_core.messages import HumanMessage, SystemMessage
 
-load_dotenv()
+config = dotenv_values()
+
+model = ChatOllama(
+    model="llama3.2:3b-instruct-fp16", 
+    temperature=0,
+)
+
+messages = [
+    SystemMessage(content="Translate the following from English into Italian"),
+    HumanMessage(content="hi!"),
+]
+
+if __name__ == "__main__":
+    print(model.invoke(messages))
